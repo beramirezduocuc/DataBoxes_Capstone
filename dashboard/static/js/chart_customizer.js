a159ff8719c4e15b8cccd736d4872c7aafd6135e
beramirez <be.ramirez@duocuc.cl>
2024-10-18 19:14:58 -0300

    Mas opciones agregadas, optimizacion de codigo y fuentes multiples de datos.

--- dashboard/static/js/chart_customizer.js
+++ dashboard/static/js/chart_customizer.js
@@ -1,10 +1,19 @@
 const get_chart_url = "http://127.0.0.1:8000/dashboard/get_chart/";
 const get_chart_types_url = "http://127.0.0.1:8000/dashboard/get_chart_types/";
+
 const chartForm = document.getElementById('chartForm');
+
 const chartTypeSelect = document.getElementById('chartTypeSelect');
 const chartColorSelect = document.getElementById('chartColorSelect');
 const detailColorSelect = document.getElementById('detailColorSelect');
-const labelChoice = document.getElementById('labelSelect')
+const labelChoice = document.getElementById('labelSelect');
+const widthChoice = document.getElementById('widthSelect');
+const detailWidthChoice = document.getElementById('detailWidthSelect');
+
+const displayWidth = document.getElementById('displayWidthSelect');
+const displayDetailWidthChoice = document.getElementById('displayDetailWidthChoice');
+
+
 
 
 const colors = [
@@ -24,33 +33,60 @@ const labelBool = [
     { value: 'false', label: 'No'},
 ]
 
-// Función para obtener un color aleatorio
+const graphTypes = [
+    { value: 'line', label: 'Lineas'},
+    { value: 'bar', label: 'Barras'},
+    { value: 'pie', label: 'Pastel'}
+];
+
 const getRandomColor = () => {
     const filteredColors = colors.filter(color => color.value !== 'random'); // Filtra el color aleatorio
     const randomColor = filteredColors[Math.floor(Math.random() * filteredColors.length)];
     return randomColor.value;
 };
 
-// Obtén los tipos de gráficos
-async function fetchChartTypes() {
-    const response = await fetch(get_chart_types_url);
-    
-    if (!response.ok) {
-        throw new Error('Error en la respuesta del servidor');
-    }
+const fieldMappings = {
+    bar: {
+        width: true,
+        detailWidth: false,
+        detailColor: false,
+        label: true,
+    },
+    line: {
+        width: true,
+        detailWidth: true,
+        detailColor: true,
+        label: true,
+    },
+    pie: {
+        width: false,
+        detailWidth: false,
+        detailColor: false,
+        label: false,
+    },
+};
 
-    const data = await response.json();
-    return data.chart_types;
-}
 
-// Llena las opciones del formulario con tipos de gráficos y colores
+
+
+const updateFormFields = () => {
+    const selectedType = chartTypeSelect.value;
+    const fields = fieldMappings[selectedType];
+
+    widthSelect.parentElement.style.display = fields.width ? 'block' : 'none';
+    detailWidthSelect.parentElement.style.display = fields.detailWidth ? 'block' : 'none';
+    detailColorSelect.parentElement.style.display = fields.detailColor ? 'block' : 'none';
+    labelChoice.parentElement.style.display = fields.label ? 'block' : 'none';
+};
+
+chartTypeSelect.addEventListener('change', updateFormFields);
+
 async function populateChartOptions() {
-    const graphTypes = await fetchChartTypes();
 
-    Object.entries(graphTypes).forEach(([type, name]) => {
+    graphTypes.forEach(graphType => {
         const option = document.createElement('option');
-        option.value = type;
-        option.textContent = name;
+        option.value = graphType.value;
+        option.textContent = graphType.label;
         chartTypeSelect.appendChild(option);
     });
         
@@ -74,40 +110,55 @@ window.addEventListener('load', populateChartOptions);
 
 // Función para obtener los valores del formulario
 const getFormValues = () => {
-    const selectedType = chartTypeSelect.value;  
+    let selectedType = chartTypeSelect.value;  
     let selectedColor = chartColorSelect.value;
     let selectedDetailColor = detailColorSelect.value;
     let selectedLabel = labelSelect.value === 'true';
+    let selectedWidth = widthChoice.value;
+    let selectedDetailWidth = detailWidthChoice.value;
 
     if (selectedColor === 'random') {
-        selectedColor = getRandomColor(); // Obtener color aleatorio
+        selectedColor = getRandomColor(); 
     }
     if (selectedDetailColor === 'random') {
-        selectedDetailColor = getRandomColor(); // Obtener color aleatorio para detalles
+        selectedDetailColor = getRandomColor(); 
     }
+    if (selectedType === 'line'){
+        selectedWidth = selectedWidth/10
+    }  
     
     return {
         selectedType,
         selectedColor,
         selectedDetailColor,
         selectedLabel,
+        selectedWidth,
+        selectedDetailWidth,
     };
 };
 
-// Evento para actualizar el gráfico cuando se cambie el formulario
 chartForm.addEventListener('change', async function() {
     const formData = getFormValues();
+    updateWidthValue();
     await initChart(formData);  
 });
 
-// Mostrar un gráfico por defecto al cargar la página
+const updateWidthValue = () => {
+    widthValueDisplay.textContent = `Ancho de línea: ${widthChoice.value}`;
+    detailWidthDisplay.textContent = `Ancho de detalles: ${detailWidthChoice.value}`;
+};
+
+
 window.addEventListener("load", async () => {
     const defaultFormData = {
-        selectedType: 'bar',
+        selectedType: 'line',
         selectedColor: '#800020',
         selectedDetailColor: '#800020',
         selectedLabel:true,
+        selectedWidth:15/10,
+        selectedDetailWidth:10,
     };
+    updateWidthValue();
     await initChart(defaultFormData);  
 });
 
@@ -125,6 +176,8 @@ const getOptionChart = async (formData) => {
                 graph_color: formData.selectedColor,
                 graph_detail: formData.selectedDetailColor,
                 graph_label: formData.selectedLabel,
+                graph_line_width: formData.selectedWidth,
+                graph_detail_width: formData.selectedDetailWidth,
             })
         });
 
@@ -169,3 +222,36 @@ function getCookie(name) {
 }
 
 const csrftoken = getCookie('csrftoken'); 
+
+
+// Contenedor para los selectores de color
+const dynamicColorSelectors = document.getElementById('dynamicColorSelectors');
+
+// Función para generar los selectores de color
+const createColorSelectors = (numSeries) => {
+    dynamicColorSelectors.innerHTML = '';  // Limpiar el contenedor antes de agregar los nuevos selectores
+
+    for (let i = 0; i < numSeries; i++) {
+        const colorSelectorDiv = document.createElement('div');
+        colorSelectorDiv.classList.add('mt-2');
+
+        const label = document.createElement('label');
+        label.textContent = `Color de gráfico para la serie ${i + 1}`;
+
+        const select = document.createElement('select');
+        select.classList.add('w-full', 'py-2', 'px-4', 'bg-white', 'border', 'rounded-lg', 'text-gray-900', 'mt-2', 'shadow-md');
+        select.id = `seriesColorSelect${i}`;
+
+        colors.forEach(color => {
+            const option = document.createElement('option');
+            option.value = color.value;
+            option.textContent = color.label;
+            select.appendChild(option);
+        });
+
+        colorSelectorDiv.appendChild(label);
+        colorSelectorDiv.appendChild(select);
+        dynamicColorSelectors.appendChild(colorSelectorDiv);
+    }
+};
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render,redirect
-from .forms import chartTypeForm
+from django.shortcuts import render
 from django.http.response import JsonResponse
 import json
 import requests
 from random import randrange
-from django.views.decorators.csrf import csrf_exempt
 
-from django.http import JsonResponse, HttpResponseBadRequest
 
+from django.http import JsonResponse
 def dashboard(request):
     username = request.user.nombre
     cloud_ping_url = 'https://southamerica-west1-databuckets-437414.cloudfunctions.net/saludar'
@@ -21,41 +19,27 @@ def dashboard(request):
                 'ola' : saludo}
     return render(request, 'dashboard/dashboard.html', context)
 
-
-
-
-GRAPH_TYPES = {
-    'bar': {
-        'name': 'Barra',
-    },
-    'line': {
-        'name': 'Línea',
-    },
-
-}
 def get_chart(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)  
-            graph_type = data.get('graph_type')
-            graph_color = data.get('graph_color')
-            graph_detail = data.get('graph_detail')  
-            graph_label = data.get('graph_label')
-            serie = [randrange(100, 400) for _ in range(7)]  # Datos aleatorios
-
+            params = {**data}
+            serie_1 = [randrange(100, 400) for _ in range(7)]  
+            serie_2 = [randrange(100, 400) for _ in range(7)] 
             chart = {
                 'grid': {
                     'left': '0%',
                     'right': '0%',
                     'top': '5%',
                     'bottom': '0%',
-                    'containLabel': graph_label
+                    'containLabel': params['graph_label'],
                 },
                 'legend': {
                     'show': True,
                     'type': 'plain',
-                    'top': '10%',  
-                    'left': 'center',  
+                    'top': '10%',  # Ajusta la posición del legend
+                    'orient': 'vert',  # Puedes ajustar esto según la necesidad
+                    'orient': 'horizontal',  # O 'vertical' si deseas que sea vertical
                 },
                 'tooltip': {
                     'show': True,
@@ -75,14 +59,32 @@ def get_chart(request):
                 ],
                 'series': [
                     {
-                        'data': serie,
-                        'type': graph_type,  
+                        'name': 'Varaiable 1',
+                        'data': serie_1,
+                        'type': params['graph_type'],  
                         'itemStyle': {
-                            'color': graph_color  
+                            'color': params['graph_detail'] if params['graph_type'] == 'line' else params['graph_color']
                         },
                         'lineStyle': {
-                            'color': graph_detail
-                        }
+                            'width': params['graph_line_width'], 
+                            'color': params['graph_color']
+                        },
+                        'symbolSize': params['graph_detail_width'],
+                        'barWidth':params['graph_line_width'] if params['graph_type'] == 'bar' else None
+                    },
+                    {
+                        'name': 'Variable 2',  # Nombre para la segunda serie
+                        'data': serie_2,  # Segundo conjunto de datos
+                        'type': params['graph_type'],
+                        'itemStyle': {
+                            'color': params['graph_detail'] if params['graph_type'] == 'line' else params['graph_color']
+                        },
+                        'lineStyle': {
+                            'width': params['graph_line_width'],
+                            'color': params['graph_color']
+                        },
+                        'symbolSize': params['graph_detail_width'] if params['graph_type'] == 'line' else 0,
+                        'barWidth': params['graph_line_width'] if params['graph_type'] == 'bar' else None
                     }
                 ]
             }
@@ -94,21 +96,11 @@ def get_chart(request):
         return JsonResponse({'error': 'Método no permitido'}, status=405)
 
 
-def get_chart_types(request):
-    if request.method == 'GET':
-        chart_types = {key: value['name'] for key, value in GRAPH_TYPES.items()}  # Devuelve un diccionario de tipos de gráficos
-        return JsonResponse({'chart_types': chart_types})
-    else:
-        return JsonResponse({'error': 'Método no permitido'}, status=405)
-
 
 
 def create_chart(request):
-
         return render(request, 'crud/create_chart.html')
 
-def bar_chart(request):
-    return render(request, 'charts/bar_chart.html')
 
 def line_chart(request):
     return render(request, 'charts/line_chart.html')
