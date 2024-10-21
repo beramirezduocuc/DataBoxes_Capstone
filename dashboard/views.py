655d79102c7b87637ee62f78529928bf15e11df1
beramirez <be.ramirez@duocuc.cl>
2024-10-21 16:57:00 -0300

    rama estable, avanzada deteccion de n variables

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,6 +18,7 @@ def dashboard(request):
     return render(request, 'dashboard/dashboard.html', context)
 
 
+<<<<<<< Updated upstream
 def get_chart(_request):
 
     colors = ['blue', 'orange', 'red', 'black', 'yellow', 'green', 'magenta', 'lightblue', 'purple', 'brown']
@@ -40,6 +41,48 @@ def get_chart(_request):
             {
                 'type': "category",
                 'data': ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+=======
+            series = [
+                [randrange(100, 400) for _ in range(7)] for _ in range(2)
+            ]
+
+            series_data = [
+                {'name': f'Variable {i+1}', 'data': serie} 
+                for i, serie in enumerate(series)
+            ]
+
+            chart = {
+                'grid': {
+                    'left': '0%',
+                    'right': '0%',
+                    'top': '5%',
+                    'bottom': '0%',
+                    'containLabel': params['graph_label'],
+                },
+                'legend': {
+                    'show': params['graph_legend_show'],
+                    'type': 'scroll',
+                    'orient': 'horizontal',  
+                },
+                'tooltip': {
+                    'show': True,
+                    'type': 'cross',
+                    'trigger': "axis",
+                    'triggerOn': "mousemove|click"
+                },
+                'xAxis': [
+                    {
+                        'type': "category",
+                        'data': ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+                    }
+                ],
+                'yAxis': [
+                    {
+                        'type': "value"
+                    }
+                ],
+                'series': []
+>>>>>>> Stashed changes
             }
         ],
         'yAxis': [
@@ -61,11 +104,67 @@ def get_chart(_request):
         ]
     }
 
+<<<<<<< Updated upstream
     return JsonResponse(chart)
 
 
 def bar_chart(request):
     return render(request, 'charts/bar_chart.html')
+=======
+            for i, serie in enumerate(series_data):
+                chart['series'].append({
+                    'name': serie['name'],
+                    'data': serie['data'],
+
+
+                    'stack': 'Total' if params.get('graph_stack', True) else None,  # Condición para gráfico apilado
+                    'areaStyle': {} if params.get('graph_stack', True) else None,   # Solo aplica si es apilado
+                    
+                    'emphasis': {
+                        'focus': 'series'
+                    } if params.get('graph_stack', True) else None,
+
+
+                    'type': params.get('graph_type'),  
+                    'itemStyle': {
+                        'color': params.get('graph_detail')[i] if params['graph_type'] == 'line' else params['graph_color'][i]
+                    },
+                    'lineStyle': {
+                        'width': params.get('graph_line_width'),
+                        'color': params['graph_color'][i] 
+                    },
+                    'symbolSize': params.get('graph_detail_width', 4),
+                    'barWidth': params.get('graph_line_width', 10) if params['graph_type'] == 'bar' else None
+                })
+            
+            response_data = {
+                'chart':chart,
+                'chartNumbers':len(series_data)
+            }
+            return JsonResponse(response_data)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Solicitud JSON malformada'}, status=400)
+        except Exception as e:
+            print("Error:", e)  
+            return JsonResponse({'error': 'Ocurrió un error al procesar la solicitud.'}, status=500)
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+
+def create_chart(request):
+    if request.method == 'POST':
+        chartNumbers = int(request.POST.get('chartNumbers')) 
+    else: 
+        chartNumbers = 5
+    chartValues = range(chartNumbers)
+    context = {
+        'chartValues': chartValues,
+        'chartNumbers': chartNumbers  
+    }
+    return render(request, 'crud/create_chart.html', context)
+
+>>>>>>> Stashed changes
 
 def line_chart(request):
     return render(request, 'charts/line_chart.html')
